cef91054efa8d2722ef237698c14608822f7aa00
Luis <admin@luiscaldas.com>
2020-05-01 20:45:23 +0100

    changed neox path so it can be linked alone

--- programs/local/startx/startx.bash
+++ programs/local/startx/startx.bash
@@ -2,17 +2,18 @@
 
 function main() {
 
+    # set proper files
     authority_file="${HOME}""/.config/xorg/XAuthority"
     init_file="${HOME}""/.xinitrc"
 
+    # get the current tty
     tty=$(tty)
     if expr "$tty" : '/dev/tty[0-9][0-9]*$' > /dev/null; then
         tty_num=$(echo "$tty" | grep -oE '[0-9]+$')
         vtarg="vt$tty_num -keeptty"
     fi
 
-    echo "$vtarg"
-
+    # execute xinit with the new files
     exec xinit "$init_file" -- "$vtarg" -auth "$authority_file" -logfile "$(mktemp)" 2>&1 | logger -t "neox"
 
 }
