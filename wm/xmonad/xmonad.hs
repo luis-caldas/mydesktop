a991e9d4f6d419e7a6501d8e4af52580f6077fd1
Luis <admin@luiscaldas.com>
2020-05-18 18:49:11 +0100

    added native application fullscreen support in xmonad

--- wm/xmonad/xmonad.hs
+++ wm/xmonad/xmonad.hs
@@ -4,8 +4,11 @@
 -- XMonad
 import XMonad
 import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
 import XMonad.Layout.LayoutModifier
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
@@ -20,6 +23,25 @@ import Data.List
 -- }}}
 
 -- {{{ Functions
+
+-- Hack to let firefox fullscreen
+setFullscreenSupported :: X ()
+setFullscreenSupported = withDisplay $ \dpy -> do
+    r <- asks theRoot
+    a <- getAtom "_NET_SUPPORTED"
+    c <- getAtom "ATOM"
+    supp <- mapM getAtom ["_NET_WM_STATE_HIDDEN"
+                         ,"_NET_WM_STATE_FULLSCREEN" -- XXX Copy-pasted to add this line
+                         ,"_NET_NUMBER_OF_DESKTOPS"
+                         ,"_NET_CLIENT_LIST"
+                         ,"_NET_CLIENT_LIST_STACKING"
+                         ,"_NET_CURRENT_DESKTOP"
+                         ,"_NET_DESKTOP_NAMES"
+                         ,"_NET_ACTIVE_WINDOW"
+                         ,"_NET_WM_DESKTOP"
+                         ,"_NET_WM_STRUT"
+                         ]
+    io $ changeProperty32 dpy r a c propModeReplace (fmap fromIntegral supp)
 
 -- Receives a DISPLAY string and returns one of the items of the 
 -- given tuple
@@ -181,14 +203,16 @@ main = do
             , borderWidth        = fromInteger $ scalePixels scaling myBorderWidth
             , focusedBorderColor = myFocusedBorderColour
             , normalBorderColor  = myNormalBorderColour
-            , layoutHook         = spacingRawScalable 10 scaling $ 
-                                   layoutHook def
-            , manageHook         = manageHook defaultConfig <+> manageDocks
+            , layoutHook         = smartBorders $ spacingRawScalable 10 scaling $ 
+                                   layoutHook defaultConfig
+            , manageHook         = manageDocks <+> manageHook defaultConfig
+            , handleEventHook    = handleEventHook defaultConfig <+> fullscreenEventHook
+			, startupHook        = startupHook defaultConfig <+> setFullscreenSupported
             , workspaces         = myWorkspaces
             } 
   
     -- Add my key bindings
-    xmobarSpawner <- spawnMyBar myBarCommand myBarPP $ myDefaultConfig
+    xmobarSpawner <- spawnMyBar myBarCommand myBarPP $ ewmh $ myDefaultConfig
                  `additionalKeysP` myKeyBindings
                  `removeKeysP`     myRemoveBindings
 
