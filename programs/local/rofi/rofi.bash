75fcaa85c86773e2825b680007132995b6f76e98
Luis <admin@luiscaldas.com>
2020-05-18 16:24:35 +0100

    added xresources and configure rofi to use it also removed visuals folder

--- programs/local/rofi/rofi.bash
+++ programs/local/rofi/rofi.bash
@@ -6,7 +6,6 @@
 
 DEFAULT_WIDTH=750
 DEFAULT_DPI=100
-DEFAULT_FONT="mono 10"
 
 #############
 # Functions #
@@ -36,30 +35,49 @@ function extract_scaling_factor() {
 
 }
 
+function extract_border() {
+    border=$(xrdb -query | grep "myvars\.border" | cut -f 2)
+    [ -z "$border" ] && border=1
+    echo -n "$border"
+}
+
+function extract_space() {
+    space=$(xrdb -query | grep "myvars\.space-inside" | cut -f 2)
+    [ -z "$space" ] && space=5
+    echo -n "$space"
+}
+
 ########
 # Main #
 ########
 
 # get the gnomes scaling factor if possible
 scaling_factor=$(extract_scaling_factor)
+border=$(extract_border)
+spacing=$(extract_space)
 
 # calculate the width and dpi with the scaling factor
 new_float_width=$(echo "$scaling_factor""*""$DEFAULT_WIDTH" | bc)
 new_float_dpi=$(echo "$scaling_factor""*""$DEFAULT_DPI" | bc)
+new_float_border=$(echo "$scaling_factor""*""$border" | bc)
+new_float_space=$(echo "$scaling_factor""*""$spacing" | bc)
 new_width=${new_float_width%.*}
 new_dpi=${new_float_dpi%.*}
+new_border=${new_float_border%.*}
+new_space=${new_float_space%.*}
+
+# get the proper char for the user running
+[ "$EUID" -ne 0 ] && user_char="$" || user_char="#"
 
 # run rofi with our configs
-rofi -show drun -modi drun -location 0 -padding 10 -width "$new_width" \
-        -lines 10 -line-margin 2 -line-padding 2 \
-        -separator-style none -font "$DEFAULT_FONT" -columns 1 \
-        -bw 1 \
+rofi -show run -display-run "$user_char " --location 0 \
+        -padding "$new_space" \
+        -width "$new_width" \
+        -lines 10 -line-margin 2 -line-padding 8 \
+        -separator-style none -columns 1 \
+        -bw "$new_border" \
         -dpi "$new_dpi" \
         -no-click-to-exit \
         -disable-history \
         -hide-scrollbar \
-        -color-window "#F2F2F2, #000000, #000000" \
-        -color-normal "#F2F2F2, #000000, #F2F2F2, #01007F, #F2F2F2" \
-        -color-active "#F2F2F2, #000000, #F2F2F2, #007763, #000000" \
-        -color-urgent "#F2F2F2, #000000, #F2F2F2, #77003d, #000000" \
         -kb-row-select "Tab" -kb-row-tab ""
