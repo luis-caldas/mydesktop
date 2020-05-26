9e64cc7b306df2c094d791a70f5555d9a622364a
Luis <admin@luiscaldas.com>
2020-05-26 21:04:46 +0100

    put all the xmonad config in a single file again

--- wm/xmonad/xmonad.hs
+++ wm/xmonad/xmonad.hs
@@ -1,12 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- {{{ Imports
-
--- Local
-import Mine.Defaults
-import Mine.Utils
 
 -- XMonad
 import XMonad
 -- import XMonad.Layout.NoBorders -- Still have to figure out how to remove borders on fullscreen
+import XMonad.Layout.Spacing
+import XMonad.Layout.LayoutModifier
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
@@ -17,6 +17,269 @@ import System.Environment
 
 -- Data
 import Data.List
+
+import Data.Maybe
+
+import qualified Data.Text
+import qualified Data.Map
+
+-- Window managing
+import qualified XMonad.StackSet
+
+-- }}}
+
+-- {{{ Configs
+
+-- Scaling
+scalingVarName = "GDK_SCALE"
+
+-- XResources
+xrCommand               = ("xrdb", ["-query"])
+
+xrVarBorder             = "xmonad.border"
+xrVarSpace              = "xmonad.space"
+xrVarBorderColour       = "xmonad.border-colour"
+xrVarBorderColourActive = "xmonad.border-colour-active" 
+xrVarBarBack            = "xmobar.background"
+xrVarBarFore            = "xmobar.foreground"
+
+-- Font
+myFontSize = 12
+myFontFace = "mono"
+
+-- My applications
+myTerminal = "APPLICATION_UNICODE=true st -f \"" ++ myFontFace ++ ":size=" ++ (show myFontSize) ++ "\""
+myBrowser  = "firefox"
+myLauncher = "neorofi"
+
+-- My borders
+myBorderWidth = 2
+
+myFocusedBorderColour = "#FFFFFF"
+myNormalBorderColour  = "#000000"
+
+-- Spacing
+myWindowSpacing = 5
+
+-- Steps for changing volume/backlight
+myKeyStep = 5
+
+-- Wokspaces
+myWorkspaces = map show [ 1 .. 9 ]
+
+-- Mod key
+myModKey = mod1Mask -- alt
+
+-- XMobar config files
+displayVar = "DISPLAY"
+
+myBar = "xmobar"
+
+myBarDefaultBack = "#FFFFFF"
+myBarDefaultFore = "#000000"
+
+myBarArguments :: Integer -> [String]
+myBarArguments scalingFactor =
+    [ "-f", "xft:" ++ myFontFace ++ ":size=" ++ (show myFontSize)
+    ]
+
+myBarColourArguments :: String -> String -> [String]
+myBarColourArguments foreColour backColour =
+    [ "-F", foreColour, "-B", backColour]
+
+myBarConfigs = ( myBarConfigFolder ++ "/main.xmobarrc"
+               , myBarConfigFolder ++ "/side.xmobarrc"
+               ) where
+                    myBarConfigFolder = "\"${HOME}\"/.config/xmobar"
+
+myBarPP = def { ppCurrent         = wrap "[" "]"
+              , ppSep             = " ||| "
+              , ppHidden          = wrap "|" "|"
+              , ppHiddenNoWindows = wrap " " " "
+              , ppLayout          = const ""
+              , ppTitle           = shorten 60
+              } 
+
+-- Picom config
+myCompositor = "picom"
+
+myCompositorArguments :: Integer -> [String]
+myCompositorArguments scalingFactor = 
+    [ "--shadow"
+    , "--shadow-opacity" , show 0.5
+    , "--shadow-radius"  , show $ scalingFactor * 10
+    , "--shadow-offset-x", show $ scalingFactor * 10
+    , "--shadow-offset-y", show $ scalingFactor * 10
+    , "--no-dock-shadow"
+    , "--fading"
+    , "--fade-delta"     , show 5
+    ]
+
+-- Commands that should be run before startup
+myStartupCommands = [ 
+                      -- Cursor setting
+                      "xsetroot -cursor_name left_ptr"
+                    ]
+-- }}}
+
+-- {{{ Keybindings
+
+-- Launching
+myKeyBindings = [ 
+                -- Spawners
+                  ("M-<Return>", spawn myTerminal)
+                , ("M-n"       , spawn myBrowser)
+                , ("M-<Space>" , spawn myLauncher)
+                -- Killer
+                , ("M-<Backspace>", kill)
+                -- Navigation
+                , ("M-["  , windows XMonad.StackSet.focusUp)
+                , ("M-/"  , windows XMonad.StackSet.focusDown)
+                , ("M-p"  , windows XMonad.StackSet.swapMaster)
+                , ("M-S-[", windows XMonad.StackSet.swapUp)
+                , ("M-S-/", windows XMonad.StackSet.swapDown)
+                -- Resizing
+                , ("M-'", sendMessage Expand)
+                , ("M-;", sendMessage Shrink)
+                -- Volume
+                , ("<XF86AudioMute>"       , spawn "pactl set-sink-mute   @DEFAULT_SINK@ toggle")
+                , ("<XF86AudioLowerVolume>", spawn ("pactl set-sink-volume @DEFAULT_SINK@ -" ++ (show myKeyStep) ++ "%"))
+                , ("<XF86AudioRaiseVolume>", spawn ("pactl set-sink-volume @DEFAULT_SINK@ +" ++ (show myKeyStep) ++ "%"))
+                -- Backlight
+                , ("<XF86MonBrightnessUp>"  , spawn ("light -A " ++ (show myKeyStep)))
+                , ("<XF86MonBrightnessDown>", spawn ("light -U " ++ (show myKeyStep)))
+                ] ++
+                -- Displays shortcut
+                [ (("M" ++ shift ++ key), screenWorkspace sc >>= flip whenJust (windows . f))
+                    | (key, sc) <- zip (map ("-"++) ["q", "w", "e", "r"]) [0..]
+                    , (f, shift) <- [ (XMonad.StackSet.view, "")
+                                    , (\f -> XMonad.StackSet.view f . XMonad.StackSet.shift f, "-S")
+                                    ]] ++
+                -- Workspaces shortcuts
+                [ (("M" ++ shift ++ key), windows $ f i)
+                    | (i, key) <- zip myWorkspaces (map ("-"++) (map show [1..9]))
+                    , (f, shift) <- [ (XMonad.StackSet.greedyView, "")
+                                    , (\i -> XMonad.StackSet.greedyView i . XMonad.StackSet.shift i, "-S")
+                                    ]]
+
+-- Bindings that should be removed
+myRemoveBindings = [ "M-S-<Return>"
+                 , "M-p"
+                 , "M-S-c"
+                 , "M-S-<Space>"
+                 , "M-,"
+                 , "M-."
+                 , "M-q"
+                 , "M-S-q"
+                 , "M-S-/"
+                 , "M-?"
+                 ] ++ 
+                 (map ("M-"++) ["h", "j", "k", "l"]) ++
+                 ["M-" ++ n | n <- ["w", "e", "r"]] ++
+                 ["M-" ++ [n] | n <- ['1'..'9']] ++
+                 ["M-S-" ++ n | n <- ["w", "e", "r"]] ++
+                 ["M-S-" ++ [n] | n <- ['1'..'9']]
+
+-- }}}
+
+-- {{{ Functions
+
+-- Hack to let firefox fullscreen
+setFullscreenSupported :: X ()
+setFullscreenSupported = withDisplay $ \dpy -> do
+    r <- asks theRoot
+    a <- getAtom "_NET_SUPPORTED"
+    c <- getAtom "ATOM"
+    supp <- mapM getAtom ["_NET_WM_STATE_HIDDEN"
+                         ,"_NET_WM_STATE_FULLSCREEN" -- XXX Copy-pasted to add this line
+                         ,"_NET_NUMBER_OF_DESKTOPS"
+                         ,"_NET_CLIENT_LIST"
+                         ,"_NET_CLIENT_LIST_STACKING"
+                         ,"_NET_CURRENT_DESKTOP"
+                         ,"_NET_DESKTOP_NAMES"
+                         ,"_NET_ACTIVE_WINDOW"
+                         ,"_NET_WM_DESKTOP"
+                         ,"_NET_WM_STRUT"
+                         ]
+    io $ changeProperty32 dpy r a c propModeReplace (fmap fromIntegral supp)
+
+-- Receives a DISPLAY string and returns one of the items of the 
+-- given tuple
+monitorConfig :: String -> (String, String) -> String
+monitorConfig display displayConfigList
+    | lastChars == ":0" = mainMonitorConfig
+    | lastChars == ".0" = mainMonitorConfig
+    | otherwise = sideMonitorConfig
+    where
+        lastChars = drop (length display - 2) display
+        mainMonitorConfig = fst displayConfigList
+        sideMonitorConfig = snd displayConfigList
+
+-- Scales pixels with a multiplier
+scalePixels :: Integer -> Integer -> Integer
+scalePixels scale inputPx =
+    scale * inputPx
+
+-- Creates a spacing that is scalable
+spacingRawScalable :: Integer -> Integer -> l a -> ModifiedLayout Spacing l a
+spacingRawScalable borderPx scalingFactor = 
+    spacingRaw False borderScaled True borderScaled True
+    where
+        borderSizeScaled = scalePixels scalingFactor borderPx
+        borderScaled = Border borderSizeScaled borderSizeScaled borderSizeScaled borderSizeScaled
+
+-- Spawn xmobar with input pipe
+spawnMyBar :: LayoutClass l Window => String -> PP -> XConfig l -> IO (XConfig (ModifiedLayout AvoidStruts l))
+spawnMyBar commandString ppIn confIn = do
+    pipe <- spawnPipe commandString
+    return $ docks $ confIn 
+        { layoutHook = avoidStruts (layoutHook confIn)
+        , logHook    = do
+                        logHook confIn
+                        dynamicLogWithPP ppIn { ppOutput = hPutStrLn pipe }
+        }
+
+-- Transforms a list of arguments into a command string
+argumentsToString :: [String] -> String
+argumentsToString argsList = 
+    intercalate " " argsList
+
+-- Adds double quotes to the beginning and end of a string
+addQuotes :: String -> String
+addQuotes stringIn =
+    "\"" ++ stringIn ++ "\""
+
+-- Extract tuple of ':' separate data from a line
+extractTupleLine :: String -> (String, String)
+extractTupleLine lineInput =
+    (name, value) 
+    where    
+        fragmented = Data.Text.splitOn (Data.Text.pack ":") (Data.Text.pack lineInput)
+        name = Data.Text.unpack $ Data.Text.strip $ head fragmented
+        value = Data.Text.unpack $ Data.Text.strip $ fullString
+            where
+                fullString = Data.Text.pack $ intercalate " " $ map Data.Text.unpack (tail fragmented)
+
+-- Transforms a xresources string into a xresources data
+xrdbParse :: String -> Data.Map.Map String String
+xrdbParse stringInput = 
+    Data.Map.fromList dataList
+    where 
+        dataList = map extractTupleLine listLines
+            where
+                listLines = lines stringInput
+
+-- Look data in a map and return it
+lookMap :: Data.Map.Map String String -> String -> String -> String
+lookMap mapInput mapSearch defaultVal =
+    fromMaybe defaultVal lookedVar
+    where 
+        lookedVar = Data.Map.lookup mapSearch mapInput
+
+-- Transforms string to integer
+mToInteger :: String -> Integer
+mToInteger strIn = 
+    read strIn::Integer
 
 -- }}}
 
