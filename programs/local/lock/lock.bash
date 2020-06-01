880a49e6279f327777677f07b7ef5cbff25efb36
Luis <admin@luiscaldas.com>
2020-06-01 19:48:00 +0100

    added auto screen blanking on lock

--- programs/local/lock/lock.bash
+++ programs/local/lock/lock.bash
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+TIMEOUT=10 # seconds
+
+# blank screen
 xset dpms force off
+
+# set dpms timeout to someting fast
+xset s "$TIMEOUT"
+
+# lock screen
 slock
+
+# restore the timeout to nothing
+xset s off
