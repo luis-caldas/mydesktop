247a78d8a47abd68e3620c982e5fc05e9de35b7a
Luis <admin@luiscaldas.com>
2020-06-09 21:39:29 +0100

    fixed scrot script

--- programs/local/scrot/scrot.bash
+++ programs/local/scrot/scrot.bash
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 SCREENSHOT_PATH="${HOME}/pics/scrots"
-PIC_EXTENSION=".png"
-NAMO=$(basename "${0}")
 
 # {{{ Utils
 
@@ -12,26 +10,20 @@ foldrizer() {
     fi
 }
 
-stamp() {
-    date +"%Y-%m-%d-%H%M%S_${NAMO}"
-}
-
 # }}}
 
 save() {
     # Create scrot folder
     foldrizer
-    # Get scrot name
-    scrot_name=$(stamp)
     # Save screen shot
-    scrot "${1}" -e 'mv $f '"${SCREENSHOT_PATH}"
+    scrot "${1}" -e "mv \$f ${SCREENSHOT_PATH}"
 }
 
 usage() {
     echo "${0}" "{s|select|-h|--help}"
 }
 
-extra_lags=""
+extra_flags=""
 case $1 in
     s|select)
         extra_flags="-s"
