181135cc17a26c0bc02343e3b66e66f3ec6eb002
Luis <admin@luiscaldas.com>
2020-09-30 23:23:58 +0100

    added fixed size for st calendar

--- programs/local/calendar/calendar.bash
+++ programs/local/calendar/calendar.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # Launches a st with given size and the cal command inside
-st -T "neocalendar" -f mono:size=12 -e sh -c 'cal -3 && read' -g "8x64"
+st \
+	-T "neocalendar" \
+	-g 76x14 \
+	-f mono:size=12 \
+	-e sh \
+	-c 'cal -3 -m --color && read'
