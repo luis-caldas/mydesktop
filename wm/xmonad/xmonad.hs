16e2326678e00284ec1a0362fe18ac4af18faea5
Luis <admin@luiscaldas.com>
2021-05-23 11:33:05 +0100

    updated gnome-calculator to right name

--- wm/xmonad/xmonad.hs
+++ wm/xmonad/xmonad.hs
@@ -83,7 +83,7 @@ myFloatingPrograms = [ ("neocalendar", doFloatAt 0.05 0.05)
                      , ("neosysinfo", doFloatAt 0.05 0.05)
                      , ("neobatt", doFloatAt 0.05 0.05)
                      , ("neonet", doFloatAt 0.05 0.05)
-                     , ("gnome-calculator", doCenterFloat)
+                     , ("Gnome-calculator", doFloat)
                      , ("glxgears", doCenterFloat)
                      ]
 
