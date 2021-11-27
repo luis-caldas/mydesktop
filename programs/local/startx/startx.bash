dbc4b89fdc21e9cdfa48b5e630d83e9cf60c3733
Luis <admin@luiscaldas.com>
2021-11-27 19:55:02 +0000

    added a new neox variable

--- programs/local/startx/startx.bash
+++ programs/local/startx/startx.bash
@@ -2,21 +2,29 @@
 
 function main() {
 
-	# set proper files
+	# Variable reasignment
+	newScale="$1"
+
+	# Set proper files
 	authority_file="${HOME}""/.config/xorg/XAuthority"
 	init_file="${HOME}""/.xinitrc"
 
-	# get the current tty
+	# Get the current tty
 	tty=$(tty)
 	if expr "$tty" : '/dev/tty[0-9][0-9]*$' > /dev/null; then
 		tty_num=$(echo "$tty" | grep -oE '[0-9]+$')
 		vtarg="vt$tty_num -keeptty"
 	fi
 
-	# change the working directory to the users home folder
+	# Change the working directory to the users home folder
 	cd "${HOME}" || exit 1
 
-	# execute xinit with the new files
+	# Export variable if it exists
+	if [ -n "$newScale" ]; then
+		systemctl --user set-environment NEW_SCALE="$newScale"
+	fi
+
+	# Execute xinit with the new files
 	exec xinit "$init_file" -- "$vtarg" -auth "$authority_file" -logfile "/dev/null" 2>&1 | logger -t "neox"
 
 }
