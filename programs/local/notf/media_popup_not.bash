3773ba239e44904e8e6482bfd7269848b0db2a6e
Luis <admin@luiscaldas.com>
2021-11-28 16:39:41 +0000

    added forgotten files

---
+++ programs/local/notf/media_popup_not.bash
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+POPUP_ID="30083"
+
+# Function to get current folder
+function get_folder() {
+
+    # get the folder in which the script is located
+    SOURCE="${BASH_SOURCE[0]}"
+
+    # resolve $SOURCE until the file is no longer a symlink
+    while [ -h "$SOURCE" ]; do
+
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+      SOURCE="$(readlink "$SOURCE")"
+
+      # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+
+    done
+
+    # the final assignment of the directory
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    # return the directory
+    echo "$DIR"
+}
+
+# Get our folder
+folder_now="$(get_folder)"
+
+# Create icon name
+function gen_icon_name () {
+	"${folder_now}/colour_icon.bash" "media-playback-${1}.svg"
+}
+
+# Get the default media status
+function get_status() {
+	playerctl status
+}
+
+# Create the notification function
+function notfy() {
+	dunstify -i "$(gen_icon_name "${1}")" "${2}" -r "${POPUP_ID}" -t 1500
+}
+
+function main() {
+
+	# Get the status
+	status_now=$(get_status)
+
+	# Check which status it is and set icon
+	icon="start"
+	[ "$status_now" == "Stopped" ] && icon="stop"
+	[ "$status_now" == "Paused" ] && icon="pause"
+
+	# Send the notification
+	notfy "${icon}" "${status_now}"
+
+}
+
+main "$@"
