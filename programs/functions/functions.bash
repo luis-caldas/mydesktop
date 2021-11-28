a50886ea78553d13cfd46e51ddea19a1c6db88d3
Luis <admin@luiscaldas.com>
2021-11-28 12:11:56 +0000

    added a few new functions

--- programs/functions/functions.bash
+++ programs/functions/functions.bash
@@ -11,3 +11,15 @@ function will {
 function memdisk {
 	watch -d -c -n1 grep -A1 Dirty /proc/meminfo
 }
+
+function lap_dock {
+	xrandr --output DP2-1 --auto --primary --right-of eDP1 --output eDP1 --auto
+	xinput map-to-output "Raydium Corporation Raydium Touch System" eDP1
+	swap-slash-and-back
+	neotrogen restore
+}
+
+function after_monitor_lap {
+	xinput map-to-output "Raydium Corporation Raydium Touch System" eDP1
+	neotrogen restore
+}
