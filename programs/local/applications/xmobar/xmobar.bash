0eb67f61de4dc1e1228cde4d302d83b57dd43e91
Luis <admin@luiscaldas.com>
2021-12-07 00:27:33 +0000

    added a bit more bling to xmobar

--- programs/local/applications/xmobar/xmobar.bash
+++ programs/local/applications/xmobar/xmobar.bash
@@ -37,6 +37,7 @@ source "${folder_now}/../../visual/xmobar-style.bash"
 
 # Create line for top bar
 top_bar="<action=\`wneocalendar\`><fc=${background},${colour1}:0> %date% </fc></action>"
+top_bar+="$(colour_arrow r "${colour0},${colour1}")"
 top_bar+="$(colour_arrow r "${colour1},${colour2}")"
 top_bar+="$(colour_arrow r "${colour2},${background}")"
 top_bar+=" }{ "
