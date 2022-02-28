7e0a35d1ade319e917ca420073096a4712efc56d
Luis <admin@luiscaldas.com>
2022-02-28 14:01:15 +0000

    changed default lock program

--- programs/local/applications/lock.bash
+++ programs/local/applications/lock.bash
@@ -9,7 +9,16 @@ xset dpms force off
 xset dpms "$TIMEOUT" "$TIMEOUT" "$TIMEOUT"
 
 # lock screen
-alock -auth pam -bg shade:mono,blur=50,shade=50 -cursor none
+	XSECURELOCK_AUTH_TIMEOUT="$TIMEOUT" \
+	XSECURELOCK_BLANK_TIMEOUT=5 \
+	XSECURELOCK_BLANK_DPMS_STATE="off" \
+	XSECURELOCK_NO_COMPOSITE=1 \
+	XSECURELOCK_DIM_OVERRIDE_COMPOSITOR_DETECTION=1 \
+	XSECURELOCK_PASSWORD_PROMPT="time" \
+	XSECURELOCK_SHOW_USERNAME=0 \
+	XSECURELOCK_SHOW_HOSTNAME=0 \
+	XSECURELOCK_AUTH_SOUNDS=1 \
+	xsecurelock
 
 # restore the timeout to nothing
-xset dpms 0 0 0 
+xset dpms 0 0 0
