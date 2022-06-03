67b16150a44da33084772f3c998faa89d42bf9ce
Luis <admin@luiscaldas.com>
2022-06-03 23:16:36 +0100

    fixed picom

--- programs/local/applications/picom.bash
+++ programs/local/applications/picom.bash
@@ -8,24 +8,24 @@ DEFAULT_BORDER=10
 
 function extract_scaling_factor() {
 
-	# run the gsettings command to get the scaling factor
+	# Run the gsettings command to get the scaling factor
 	factor=$(gsettings get org.gnome.desktop.interface scaling-factor 2>/dev/null || false)
 
-	# if the variable is not empty, extract its value, if not assume the scaling is one
+	# If the variable is not empty, extract its value, if not assume the scaling is one
 	if [ -n "$factor" ]; then
 	factor=$(echo "$factor" | awk '{print $NF}')
 	fi
 
-	# check if a few sys vars exist
+	# Check if a few sys vars exist
 	[ -z "$factor" ] || [ "$factor" -lt 1 ] && factor="$GDK_SCALE"
 	[ -z "$factor" ] || [ "$factor" -lt 1 ] && factor="$ELM_SCALE"
 	[ -z "$factor" ] || [ "$factor" -lt 1 ] && factor="$QT_AUTO_SCREEN_SCALE_FACTOR"
 
-	# if nothing was set until now
-	# set it to one
+	# If nothing was set until now
+	# Set it to one
 	[ -z "$factor" ] || [ "$factor" -lt 1 ] && factor="1"
 
-	# return the scaling factor
+	# Return the scaling factor
 	echo -n "$factor"
 
 }
@@ -34,15 +34,24 @@ function extract_scaling_factor() {
 # Main #
 ########
 
-# get the gnomes scaling factor if possible
+# Get the gnomes scaling factor if possible
 scaling_factor=$(extract_scaling_factor)
 
-# calculate the width and dpi with the scaling factor
+# Calculate the width and dpi with the scaling factor
 new_float_border=$(echo "$scaling_factor""*""$DEFAULT_BORDER" | bc)
 new_border=${new_float_border%.*}
 
-# run picom with custom settings
+# Create the conf for picom
+read -r -d '' conf_file << EOF
+wintypes:
+{
+  dock = { shadow = false; }
+};
+EOF
+
+# Run picom with custom settings
 picom \
+	--conf <(cat <<< "${conf_file}") \
 	--shadow \
 	--shadow-opacity 0.5 \
 	--shadow-radius "$new_border" \
@@ -50,6 +59,5 @@ picom \
 	--shadow-offset-y "$new_border" \
 	--shadow-exclude "bounding_shaped" \
 	--shadow-exclude "i:e:Conky" \
-	--no-dock-shadow \
 	--fading \
 	--fade-delta 5
